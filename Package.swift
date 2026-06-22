107907c91da3b0e3e9aa6a20243e45f71c23833c
Gonzalo Larralde <132825+gonzalolarralde@users.noreply.github.com>
2026-06-21 23:22:44 -0400

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppServerClient",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppServerClient",
+            targets: ["AppServerClient"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMajor(from: "1.12.2")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMajor(from: "1.12.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppServerClient",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]
+        ),
+        .testTarget(
+            name: "AppServerClientTests",
+            dependencies: ["AppServerClient"]
+        ),
+    ]
+)
